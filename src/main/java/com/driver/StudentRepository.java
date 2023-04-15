7371eb772d3f25be75fb68d63ab3203c38a556a3
Brajendra702 <vijendras452@gmail.com>
2023-04-15 16:48:45 +0530

    solution 3

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -8,68 +8,86 @@ import java.util.List;
 
 @Repository
 public class StudentRepository {
-    HashMap<String ,Student> studentHashMap=new HashMap<>();
-    HashMap<String ,Teacher> teacherHashMap=new HashMap<>();
-    HashMap<String, List<String>> teacherStudentHashMap=new HashMap<>();
+    HashMap<String,Student> studentDb = new HashMap<>();
+    HashMap<String,Teacher> teacherDb = new HashMap<>();
+    HashMap<Teacher,List<Student>> studentTeacher = new HashMap<>();
 
-    public void addStudent(Student student){
-         studentHashMap.put(student.getName(), student);
+
+    public void addStudent(Student student)
+    {
+        studentDb.put(student.getName(), student);
     }
 
-    public void addTeacher(Teacher teacher) {
-
-        teacherHashMap.put(teacher.getName(),teacher);
+    public void addTeacher(Teacher teacher)
+    {
+        teacherDb.put(teacher.getName(), teacher);
     }
-
-    public void addStudentTeacherPair(String student, String teacher) {
-       List<String> studentCombination=new ArrayList<>();
-       if(studentHashMap.containsKey(student) && teacherHashMap.containsKey(teacher)){
-           if(teacherHashMap.containsKey(teacher)) studentCombination=teacherStudentHashMap.get(teacher);
-           studentCombination.add(student);
-           teacherStudentHashMap.put(teacher,studentCombination);
-           //for particular teacher how many student he is traching
-           Teacher t=teacherHashMap.get(teacher);
-           t.setNumberOfStudents(studentCombination.size());
-       }
-
-    }
-
-    public void deleteAllTeachers() {
-        for(String tname:teacherHashMap.keySet()){
-            if(teacherStudentHashMap.containsKey(tname)){
-                for(String sname:teacherStudentHashMap.get(tname))
-                    studentHashMap.remove(sname);
-            }
-            else{
-                teacherHashMap.remove(tname);
-            }
+    public void addStudentTeacherPair(String studentName, String teacherName)
+    {
+        Teacher teacher = teacherDb.get(teacherName);
+        Student student = studentDb.get(studentName);
+        if(studentTeacher.containsKey(teacher))
+        {
+            List<Student> students = studentTeacher.get(teacher);
+            students.add(student);
+            studentTeacher.put(teacher,students);
+        }
+        else
+        {
+            List<Student> students = new ArrayList<>();
+            students.add(student);
+            studentTeacher.put(teacher,students);
         }
     }
 
-    public void deleteTeacherByName(String teacher) {
-        if(teacherStudentHashMap.containsKey(teacher) && teacherHashMap.containsKey(teacher)){
-            for (String sname:teacherStudentHashMap.get(teacher))
-                studentHashMap.remove(sname);
-            teacherStudentHashMap.remove(teacher);
+    public List<Student> getAllStudents()
+    {
+        List<Student> students = new ArrayList<>();
+        for(Student student: studentDb.values())
+        {
+            students.add(student);
         }
+        return students;
     }
 
-    public List<String> getAllStudent() {
-        return  new ArrayList<>(studentHashMap.keySet());
+    public void deleteTeacherByName(String name)
+    {
+        Teacher teacher = teacherDb.get(name);
+        teacherDb.remove(name);
+        for(Student student: studentTeacher.get(teacher))
+        {
+            studentDb.remove(student.getName());
+        }
+        studentTeacher.remove(teacher);
     }
 
-    public List<String> getStudentsByTeacherName(String teacher) {
-        List<String>studentList=new ArrayList<>();
-        if(teacherStudentHashMap.containsKey(teacher))
-            studentList=teacherStudentHashMap.get(teacher);
-        return studentList;
+    public void deleteAllTeachers()
+    {
+        for(Teacher teacher: teacherDb.values())
+        {
+            for(Student student: studentTeacher.get(teacher))
+            {
+                studentDb.remove(student.getName());
+            }
+        }
+        studentTeacher.clear();
+        teacherDb.clear();
     }
 
-    public Teacher getTeacherByName(String name) {
-        return teacherHashMap.get(name);
+    public List<Teacher> getAllTeachers()
+    {
+        List<Teacher> teachers = new ArrayList<>();
+        for(Teacher teacher: teacherDb.values())
+        {
+            teachers.add(teacher);
+        }
+        return teachers;
     }
 
-    public Student getStudentByName(String name) {
-        return studentHashMap.get(name);
+    public List<Student> getStudentByTeacherName(String teacherName)
+    {
+        if(studentTeacher.containsKey(teacherDb.get(teacherName)))
+            return studentTeacher.get(teacherDb.get(teacherName));
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -3,37 +3,81 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StudentService {
     @Autowired
-    StudentRepository studentRepository;
-    public void addStudent(Student student){
+    StudentRepository studentRepository = new StudentRepository();
+
+    public void addStudents(Student student)
+    {
         studentRepository.addStudent(student);
     }
-    public void addTeacher(Teacher teacher){
+
+    public void addTeacher(Teacher teacher)
+    {
         studentRepository.addTeacher(teacher);
     }
-    public void addStudentTeacherPair(String student,String teacher){
-        studentRepository.addStudentTeacherPair(student,teacher);
+    public void addStudentTeacherPair(String studentName, String teacherName)
+    {
+        studentRepository.addStudentTeacherPair(studentName,teacherName);
     }
-    public Student getStudentByName(String name){
-        return studentRepository.getStudentByName(name);
+
+    public Student getStudentByName(String name)
+    {
+        List<Student> students = studentRepository.getAllStudents();
+        for(Student student: students)
+        {
+            if(student.getName().equals(name))
+                return student;
+        }
+        return null;
+
     }
-    public Teacher getTeacherByName(String name){
-        return studentRepository.getTeacherByName(name);
+
+    public Teacher getTeacherByName(String name)
+    {
+        List<Teacher> teachers = studentRepository.getAllTeachers();
+        for(Teacher teacher: teachers)
+        {
+            if(teacher.getName().equals(name))
+                return teacher;
+        }
+        return null;
     }
-    public List<String> getStudentsByTeacherName(String teacher){
-        return studentRepository.getStudentsByTeacherName(teacher);
+
+    public List<String> getStudentByTeacherName(String teacherName)
+    {
+        List<Student> students = studentRepository.getStudentByTeacherName(teacherName);
+        List<String> studentsByTeacherName = new ArrayList<>();
+        for(Student student: students)
+        {
+            studentsByTeacherName.add(student.getName());
+        }
+        return studentsByTeacherName;
     }
-    public List<String> getAllStudent(){
-        return studentRepository.getAllStudent();
+
+    public List<String> getAllStudents()
+    {
+        List<Student> students = studentRepository.getAllStudents();
+        List<String> studentNames = new ArrayList<>();
+        for(Student student: students)
+        {
+            studentNames.add(student.getName());
+        }
+        return studentNames;
     }
-    public void deleteTeacherByName(String teacher){
+
+    public void deleteTeacherByName(String teacher)
+    {
+
         studentRepository.deleteTeacherByName(teacher);
     }
-    public void deleteAllTeachers(){
+    public void deleteAllTeachers()
+    {
+
         studentRepository.deleteAllTeachers();
     }
 }
